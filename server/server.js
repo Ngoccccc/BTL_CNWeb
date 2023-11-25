bd08e408066b01c83828c5ed1e6d0004ead0b370
Ngoccccc <trantienngoc2911@gmail.com>
2023-11-25 22:21:25 +0700

    fix code

---
+++ server/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import cors from "cors";
+
+//configure env
+dotenv.config();
+
+//database config
+connectDB();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+//routes
+
+
+//PORT
+const PORT = process.env.PORT || 8080;
+
+//run listen
+app.listen(PORT, () => {
+    console.log(
+        `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`
+    );
+});
